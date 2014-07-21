ba8bd8f43ac62f6405e163698ac3551ed699f9b3
Necko75 <dominique@MacBook-Air-de-dominique.local>
2014-07-21 22:53:32 +0200

    create function timeout for research, controller search

--- public/javascripts/FriendsMusics/controllers/controller_search.js
+++ public/javascripts/FriendsMusics/controllers/controller_search.js
@@ -2,8 +2,25 @@ playlistModule.controller('Search', function Search($scope, $timeout, $location,
 
 	$scope.socket = playlistStorage.getSocket();
 	$scope.oldMusics = $rootScope.musics;
+	$scope.timeoutSearch = undefined;
 
 	$scope.$watch('search_song', function (val) {
-		;
+
+		if (val && val.length < 3)
+      		return (false);
+    	saisie = val;
+    	if ($scope.timeoutSearch > 0 || $scope.timeoutSearch == undefined)
+    	{
+      		clearTimeout($scope.timeoutSearch);
+      		$scope.timeoutSearch = setTimeout(function() {
+        		$scope.timeoutSearch = undefined;
+        		$scope.submitSearch(saisie);
+      		}, 1000);
+    	}
 	});
+
+	$scope.submitSearch = function(saisie) {
+		console.log("recherche : " + saisie);
+	}
+
 });
