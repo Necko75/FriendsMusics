2b69b21eb4d0723b4318dfa53128b31cbb69af20
Necko75 <dominique@MacBook-Air-de-dominique.local>
2014-07-21 22:15:09 +0200

    remove function search controller, add button add playlist

--- public/javascripts/FriendsMusics/controllers/controller_search.js
+++ public/javascripts/FriendsMusics/controllers/controller_search.js
@@ -4,35 +4,6 @@ playlistModule.controller('Search', function Search($scope, $timeout, $location,
 	$scope.oldMusics = $rootScope.musics;
 
 	$scope.$watch('search_song', function (val) {
-		if (val && val.length >= 3)
-		{
-			var found = $filter('filter')($scope.oldMusics, {author : val}, false);
-			if (found.length)
-			{
-				$rootScope.musics = found;
-				$timeout(function() {
-					$scope.divholder.jPages("destroy");
-					$scope.divholder.jPages({
-						containerID : "tracksListMusics",
-						perPage : 8,
-					});
-				}, 0);
-			}
-			else 
-			{
-				found = $filter('filter')($scope.oldMusics, {name : val}, false);
-				if (found.length)
-				{
-					$rootScope.musics = found;
-				}
-				else
-					console.log('no result');
-			}
-		}
-		else
-		{
-			// il nous faut réactuliser la liste //
-			$rootScope.musics = $scope.oldMusics;
-		}
+		;
 	});
 });
